67584c31cbbd801af937af0bf11136b3718655c9
Steve <steven.beliveau2@gmail.com>
2023-03-04 10:07:36 -0500

    clean cache

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import Nav from "./component/Nav"
+import Main from "./component/Main"
+import Footer from "./component/Footer"
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Lil' 💩 React-App</h1>
+      <Nav />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+// export default App
