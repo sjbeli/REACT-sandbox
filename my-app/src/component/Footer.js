21811f80885eb2052e4ff9226e5816649d9966e0
Steve <steven.beliveau2@gmail.com>
2023-03-04 10:28:17 -0500

    reupload files - again - again

---
+++ my-app/src/component/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        &copy;{new Date().getFullYear()} <a href="https://github.com/sjbeli/react-sandbox">sjbeli</a> This be the footer section
+      </p>
+    </footer>
+  )
+}
+// export default Footer
---
+++ my-app/src/component/Main.js
@@ -0,0 +1,19 @@
+export default function Main() {
+  return (
+    <main>
+      <h2>
+        Lil' list of <a href="https://reactjs.org/">React Facts</a>
+      </h2>
+      <ul>
+        <li>Was first released in 2013 (yea, so?)</li>
+        <li>Over 100k starts on GitHub (pffft!)</li>
+        <li>Created, maintained by Facebook 🤮</li>
+        <li>Powers k's of enterprise apps, including mobile apps</li>
+        <li>And - I can do this react 💩! 😳</li>
+        <li>Is it worth my time? 🤔 🤨</li>
+      </ul>
+    </main>
+  )
+}
+
+// export default Main
