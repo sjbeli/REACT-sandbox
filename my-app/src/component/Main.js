5628d7fea1eae26d53f4f82ee99b7168b92c88c9
Steve <steven.beliveau2@gmail.com>
2023-03-04 11:38:47 -0500

    just tweakin stuff

--- my-app/src/component/Main.js
+++ my-app/src/component/Main.js
@@ -5,12 +5,14 @@ export default function Main() {
         Lil' list of stupid <a href="https://reactjs.org/">React Facts</a>
       </h2>
       <ul>
-        <li>Was first released in 2013 (yea, bfd!)</li>
-        <li>Over 100k starts - GitHub (🤣 pfft!)</li>
-        <li>Created, maintained by FB 🪓 🤮</li>
-        <li>Powers 💩loads of enterprise apps, incl mobile apps</li>
-        <li>And - I can do this react 💩! 😳</li>
-        <li>Is it worth my time? 🤔 🤨</li>
+        <li>First released in 2013</li>
+        <li>Over 100k starts - GitHub 🤣 🤓</li>
+        <li>Created, maintained by FB 🤮</li>
+        <li>Powers 💩loads of enterprise / mobile apps</li>
+        <li>I can do this react 💩! 😳</li>
+        <li>
+          really But is it <i>really worth</i> my time? 🤔 🤨
+        </li>
       </ul>
     </main>
   )
