e4209280e2bab15c7118bf87405a7704d1b76419
Steve <steven.beliveau2@gmail.com>
2023-03-04 10:39:22 -0500

    add remarks

--- my-app/src/component/Main.js
+++ my-app/src/component/Main.js
@@ -2,13 +2,13 @@ export default function Main() {
   return (
     <main>
       <h2>
-        Lil' list of <a href="https://reactjs.org/">React Facts</a>
+        Lil' list of stupid <a href="https://reactjs.org/">React Facts</a>
       </h2>
       <ul>
-        <li>Was first released in 2013 (yea, so?)</li>
-        <li>Over 100k starts on GitHub (pffft!)</li>
-        <li>Created, maintained by Facebook 🤮</li>
-        <li>Powers k's of enterprise apps, including mobile apps</li>
+        <li>Was first released in 2013 (yea, bfd!)</li>
+        <li>Over 100k starts - GitHub (🤣 pfft!)</li>
+        <li>Created, maintained by FB 🪓 🤮</li>
+        <li>Powers 💩loads of enterprise apps, incl mobile apps</li>
         <li>And - I can do this react 💩! 😳</li>
         <li>Is it worth my time? 🤔 🤨</li>
       </ul>
